03ba11a6ebb8abc002c3201a62dbfda8c9b98817
schou <pschou@users.noreply.github.com>
2021-04-15 22:06:01 -0400

    reworking methods for connection

--- main.go
+++ main.go
@@ -3,7 +3,7 @@ package main
 import (
 	"bytes"
 	"fmt"
-	"github.com/greenpau/go-cisco-nx-api/pkg/client"
+	"github.com/pschou/go-cisco-nx-api/pkg/client"
 	"github.com/pschou/go-params"
 )
 
@@ -26,27 +26,49 @@ func main() {
 
 			cli := client.NewClient()
 			cli.SetHost(host)
-			cli.SetPort(qryConf.Port)
-			cli.SetProtocol(qryConf.Protocol)
 			cli.SetUsername(qryConf.User)
 			cli.SetPassword(qryConf.Password)
+			cli.SetProtocol(qryConf.Protocol)
+			cli.SetPort(qryConf.Port)
 
-			// Get system information
-			SysInfo, err := cli.GetSystemInfo()
+			results, err := cli.GetGeneric([]string{
+				"show version",          //done
+				"show bgp session",      //done
+				"show ip route",         //done
+				"show ip arp",           //done
+				"show interface status", //done
+				"show interface quick",  //almost, have brief not quick
+				"show isis adj detail",  //almost, have isis 2 adj detail
+			})
+
 			if err != nil {
-				fmt.Println("Error connecting to host:", host, "with error:", err)
+				fmt.Println("Error in call to API for host", host, "err", err)
 				continue
 			}
 
+			//for _, result := range results {
+			//	fmt.Printf("data %#v\n", string(result.Result)) // DEBUG
+			//}
+
+			bgp_resp, err := client.NewBGPSessionFromBytes(results[1].Result)
+			if err != nil {
+				fmt.Println("Error in parsing BGP sessions for host", host, "err", err)
+				continue
+			}
+
+			bgp_slices := bgp_resp.Flat()
+			for _, bgp_slice := range bgp_slices {
+				fmt.Printf("neighbor === %#v\n", bgp_slice)
+			}
 			// Print metric
-			buf.WriteString(fmt.Sprintf("CiscoProm_up{version=%q,host=%q} 1\n",
-				SysInfo.Bios.Version, host))
+			//buf.WriteString(fmt.Sprintf("CiscoProm_up{version=%q,host=%q} 1\n",
+			//	SysInfo.Bios.Version, host))
 
 			if config.Push == "" {
 				// Print out the result
-				fmt.Printf("metrics:\n%s", buf)
+				fmt.Printf("metrics:\n%s", buf.String())
 			} else {
-				UploadToCollector(config.Push, buf.String())
+				//UploadToCollector(config.Push, buf.String())
 			}
 
 		}
